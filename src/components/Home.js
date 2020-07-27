8eb543367614bb8b72a2c4ad81d484720e9f2656
alicjakardynal <k.alicja66@gmail.com>
2020-07-27 17:24:24 +0200

    bernesestart

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+
+
+ class Main extends Component{
+     render(){
+         return(
+             <>
+            <video width="320" height="240" loop >
+    <source src="68b791eb-1d11-4276-a79b-64163eaa2f28.MP4" type="video/mp4" />
+
+    Your browser does not support the video tag.
+</video>
+dghj
+             </>
+
+         )
+     }
+ }
+
+
+
+export default Main;
