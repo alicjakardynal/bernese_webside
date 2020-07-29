1b6e50b3be52313fcba927b0ad41f0fb764a2fa5
alicjakardynal <k.alicja66@gmail.com>
2020-07-29 11:11:55 +0200

    video

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,10 @@ import React, { Component } from "react";
      render(){
          return(
              <>
-            <video width="320" height="240" loop >
-    <source src="68b791eb-1d11-4276-a79b-64163eaa2f28.MP4" type="video/mp4" />
+            <video width="1020" height="640" controls autoPlay loop muted >
+            <source src="../videos/68b791eb-1d11-4276-a79b-64163eaa2f28.MP4" type="video/mp4" />
 
+   
     Your browser does not support the video tag.
 </video>
 dghj
