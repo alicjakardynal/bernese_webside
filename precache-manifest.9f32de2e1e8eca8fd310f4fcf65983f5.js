2d89ddb2e382952dfe3674e2d3ace2111046d740
Alicja Kardynal <kardynalalicjaa@gmail.com>
2020-08-03 11:30:13 +0200

    Updates

---
+++ precache-manifest.9f32de2e1e8eca8fd310f4fcf65983f5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae3e8d5aac0ef2abac4f3c212f29516",
+    "url": "/bernese_webside/index.html"
+  },
+  {
+    "revision": "ce4c3dee927c12cbd589",
+    "url": "/bernese_webside/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "4808417eaa79af418f68",
+    "url": "/bernese_webside/static/css/main.6556e1e5.chunk.css"
+  },
+  {
+    "revision": "ce4c3dee927c12cbd589",
+    "url": "/bernese_webside/static/js/2.ea909485.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bernese_webside/static/js/2.ea909485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4808417eaa79af418f68",
+    "url": "/bernese_webside/static/js/main.4b1dcf47.chunk.js"
+  },
+  {
+    "revision": "b35ab14ef57397a47a6d",
+    "url": "/bernese_webside/static/js/runtime-main.d9496a26.js"
+  },
+  {
+    "revision": "1ea6659e28e4aff32fa9e785ab124ed2",
+    "url": "/bernese_webside/static/media/5f1edd50c359d.1ea6659e.png"
+  },
+  {
+    "revision": "3bc887c72f98ce5ce18ce98199ac2411",
+    "url": "/bernese_webside/static/media/DSC01032.3bc887c7.JPG"
+  },
+  {
+    "revision": "5a615d788a2a53425213fae853b8c610",
+    "url": "/bernese_webside/static/media/DSC01044.5a615d78.JPG"
+  },
+  {
+    "revision": "b562fa55de4943d95b5a76dee46ad5ba",
+    "url": "/bernese_webside/static/media/DSC01063.b562fa55.JPG"
+  },
+  {
+    "revision": "c1d9c65eecca410564bdc2bc11b1863d",
+    "url": "/bernese_webside/static/media/DSC01357.c1d9c65e.JPG"
+  },
+  {
+    "revision": "384b0c981a2c681cdd90b1c4ef0a6009",
+    "url": "/bernese_webside/static/media/DSC01399.384b0c98.JPG"
+  },
+  {
+    "revision": "cb2513e5b678b12c41c33bb3eb7f49f7",
+    "url": "/bernese_webside/static/media/DSC01448.cb2513e5.JPG"
+  },
+  {
+    "revision": "daf691a6338531f945d769867d2b8ac1",
+    "url": "/bernese_webside/static/media/DSC01481.daf691a6.JPG"
+  },
+  {
+    "revision": "25319e74d78c2d492801310e3ee4d412",
+    "url": "/bernese_webside/static/media/DSC01494.25319e74.JPG"
+  },
+  {
+    "revision": "00b151d47ca270af873cbf7954d4df78",
+    "url": "/bernese_webside/static/media/IMG-1443.00b151d4.jpg"
+  },
+  {
+    "revision": "3090345447b9f73a1c9aa42396da4c1c",
+    "url": "/bernese_webside/static/media/IMG_20200724_175606 — kopia.30903454.jpg"
+  },
+  {
+    "revision": "4909260e72c56233002d2ad162ff30ee",
+    "url": "/bernese_webside/static/media/VID_20200719_134749.4909260e.mp4"
+  }
+]);
